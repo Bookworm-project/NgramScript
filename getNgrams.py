f8a5ca3cdd2f8612713efdee17f6ed22ded29b9b
Muhammad S Shamim <sa501428@gmail.com>
2015-05-27 13:27:54 -0500

    Parsing and Saving Updates
    Fixed to handle Ngram Viewer Structure Updates

--- getNgrams.py
+++ getNgrams.py
@@ -1,14 +1,14 @@
-import urllib,re,sys
+#!/usr/bin/env python
+
+import urllib,re,sys,csv,os
 
 INFO="""
-JB Michel for the Harvard Cultural Observatory, Oct 19 2012 www.culturomics.org, @culturomics, @jb_michel. License: None, please distribute, modify and improve as you see fit. 
-
-This is a basic python code to retrieve data behind trajectories plotted on the Google Books Ngram Viewer: books.google.com/ngrams.
-
-Just type exactly the same string you would have type on books.google.com/ngrams, and retrieve the data in tsv format. By default, data is printed on screen and saved to file in the current directory.
+Python code to retrieve data behind trajectories plotted on the Google Books Ngram Viewer: books.google.com/ngrams.
+Type exactly the same string you would have type on books.google.com/ngrams, and retrieve the data in csv/tsv format.
+By default, data is printed on screen and saved to file in the current directory.
 
 Note to savvy users: 
-(1) you can directly pass queries as arguments to the python script, such as "python getNgrams.py awesome" or "getNgrams.exe great". 
+(1) you can directly pass queries as arguments to the python script, such as "python getNgrams.py awesome"
 (2) if you pass the '-quit' flag as an argument, the program will run once and quit without asking for more input, such as "python getNgrams.py awesome, sauce -quit". 	
 (3) Known caveat: quotation marks are removed from the input query. 
 
@@ -31,6 +31,8 @@ Flags:
      smoothing parameter (integer). Minimum is 0. 
   -nosave
      results will not be saved to file.
+  -csv
+     results will be saved to a csv file (default is tsv).
   -noprint
      results will not be printed on screen.
   -help
@@ -44,7 +46,11 @@ Possible corpora:
   spa_2012, spa_2009, rus_2012, rus_2009, heb_2012, heb_2009, ita_2012,	
   eng_fiction_2012, eng_fiction_2009, eng_1m_2009
 
-PLEASE do respect the terms of service of the Google Books Ngram Viewer while using this code. This code is meant to help viewers retrieve data behind a few queries, not bang at Google's  servers with thousands of queries. The complete dataset can be freely downloaded here: http://storage.googleapis.com/books/ngrams/books/datasetsv2.html. This code is not a Google product and is not endorsed by Google in any way. 
+PLEASE do respect the terms of service of the Google Books Ngram Viewer while using this code.
+This code is meant to help viewers retrieve data behind a few queries, not bang at Google's  servers with thousands of queries.
+The complete dataset can be freely downloaded here: http://storage.googleapis.com/books/ngrams/books/datasetsv2.html.
+
+This code is not a Google product and is not endorsed by Google in any way. 
 
 With this in mind... happy plotting!
 """
@@ -55,40 +61,64 @@ corpora={'eng_us_2012':17, 'eng_us_2009':5, 'eng_gb_2012':18, 'eng_gb_2009':6,
 	'ger_2012':20, 'ger_2009':8, 'heb_2012':24, 'heb_2009':9, 
 	'spa_2012':21, 'spa_2009':10, 'rus_2012':25, 'rus_2009':12, 'ita_2012':22}
 
+
+def extractCleanTerms(regExpression, filterTerms, fullText):
+
+	foundSections=re.findall(regExpression, fullText)
+
+	for index in range(len(foundSections)):
+		for filterTerm in filterTerms:
+			foundSections[index] = re.sub(filterTerm, '', foundSections[index])
+
+	return foundSections
+
+
 def getNgrams(query, corpus, startYear, endYear, smoothing):
 	urlquery = urllib.quote_plus(query, safe='"')
 	corpusNumber=corpora[corpus]
 	url = 'http://books.google.com/ngrams/graph?content=%s&year_start=%d&year_end=%d&corpus=%d&smoothing=%d&share='%(urlquery,startYear,endYear,corpusNumber,smoothing)
 	response = urllib.urlopen( url ).read()
 	
-	res=re.findall("data.addColumn(.*?);", response)
-	terms=[r[:-1].split(',')[1].replace("'","").strip() for r in res]
-		
-	res = re.findall("data.addRows(.*?);", response.replace('\n',''))
-	data = [[float(c) for c in r[1:-1].split(',')] for r in re.findall("\[.*?\]", res[0][1:].strip()[1:-1:])]
+	timeseries = extractCleanTerms("\"timeseries\": \[.*?\]",["\"timeseries\": \[","\]"],response)
+	terms_searched = extractCleanTerms("\{\"ngram\": \".*?\"",["\{\"ngram\": \"","\""],response)
 	
-	return url, urlquery, [terms]+data
+	terms_to_timeseries = {}
+	for index in range(len(terms_searched)):
+		terms_to_timeseries[terms_searched[index]] = [float(time) for time in timeseries[index].split(",")]
 
-def saveData(fname, data, url):
-	fo = open(fname, 'w')
-	fo.write(url+'\n')
-	for d in data:
-		try:
-			fo.write('%d,'%int(d[0])+','.join([str(s) for s in d[1:]])+'\n')
-		except:
-			fo.write(','.join([str(s) for s in d])+'\n')
-	fo.close()
+	return url, urlquery, terms_to_timeseries
+
+
+
+def saveData(fname, data, url, outputAsTSV):
+	# write to csv first (can't directly write using tab delimeters with csvwriter)
+	# then convert to tsv and delete the csv file
+	
+	outputFile = open(fname+".csv", 'w')
+	writer = csv.writer(outputFile)
+	writer.writerow([url])
+	for key, value in data.items():
+		writer.writerow([key] + value)
+	outputFile.close()
+
+	if(outputAsTSV):
+		csv.writer(file(fname+".tsv", 'w+'), delimiter="\t").writerows(csv.reader(open(fname+".csv")))
+		os.remove(fname+".csv")
+	
+
 
 def runQuery(argumentString):
 	arguments = argumentString.split()
 	query = ' '.join([arg for arg in arguments if not arg.startswith('-')])
 	params = [arg for arg in arguments if arg.startswith('-')]
-	printHelp, toSave, toPrint,corpus,startYear,endYear,smoothing=False, True,True, 'eng_2012',1800,2000,3
+	printHelp, toSave, toTSV, toPrint,corpus,startYear,endYear,smoothing=False, True, True, True, 'eng_2012',1800,2000,3
 	
 	# parsing the query parameters
 	for param in params:
 		if '-nosave' in param:
 			toSave=False
+		elif '-csv' in param:
+			toTSV=False
 		elif '-noprint' in param:
 			toPrint=False
 		elif '-corpus' in param:
@@ -112,14 +142,10 @@ def runQuery(argumentString):
 		url, urlquery,data = getNgrams(query, corpus, startYear, endYear, smoothing)
 		if toPrint:
 			print url
-			for d in data:
-				try:
-					print '%d,'%int(d[0])+','.join([str(s) for s in d[1:]])
-				except:
-					print ','.join([str(s) for s in d])
+			print data
 		if toSave:
-			filename='%s-%s-%d-%d-%d.tsv'%(urlquery,corpus,startYear,endYear,smoothing)
-			saveData(filename,data,url)
+			filename='%s-%s-%d-%d-%d'%(urlquery,corpus,startYear,endYear,smoothing)
+			saveData(filename,data,url,toTSV)
 			print 'Data saved to %s'%filename
 
 if __name__ == '__main__':
